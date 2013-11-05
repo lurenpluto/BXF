2041b6b98b9b8a8e843a5685e3068a0cf58d64a7
nifei <nieinifei@gmail.com>
2013-11-05 11:59:32 +0800

    remove unused methods

--- src/XLUEExtObject/GaussianBlurObject/GaussianBlurDelegate.h
+++ src/XLUEExtObject/GaussianBlurObject/GaussianBlurDelegate.h
@@ -6,17 +6,7 @@
 #ifndef _GAUSSIANBLURDELEGATE_H_
 #define _GAUSSIANBLURDELEGATE_H_
 
-//#define DEBUG
-
 void DericheIIRRenderSSE(XL_BITMAP_HANDLE hBitmap, const float &sigma);
 void OneDimentionRenderMMX(XL_BITMAP_HANDLE hBitmpa, const float &sigma, int radius);
-
-#ifdef DEBUG
-void DericheIIRRenderSSEIntrinsics(XL_BITMAP_HANDLE hBitmap, float m_sigma);
-void DericheIIRRender(XL_BITMAP_HANDLE hBitmap, float m_sigma);
-void TwoDimentionRender(XL_BITMAP_HANDLE hBitmap, float m_sigma, int m_radius);
-void OneDimentionRender(XL_BITMAP_HANDLE hBitmap, float m_sigma, int m_radius);
-void OneDimentionRenderSSE(XL_BITMAP_HANDLE hBitmap, float m_sigma, int m_radius);
-#endif
 
 #endif //_GAUSSIANBLURDELEGATE_H_
